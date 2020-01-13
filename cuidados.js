f2349fdfa46efe8bbed5d52719830ca0d6d50af5
Charles <47333478+ORaShhi@users.noreply.github.com>
2020-01-13 10:42:23 -0300

    Add files via upload

---
+++ cuidados.js
@@ -0,0 +1,8 @@
+console.log(7/0) // infinity
+console.log("10"/2)
+console.log("show!"*2)
+console.log(0.1 + 0.7)
+//console.log(10.toString())
+console.log((10.345).toFixed(2))
+console.log('3' + 2) //string ganha, preferencia da string (+ tem sentido em string para concaternaçao)
+console.log('3' - 2) //faz a operaçao
