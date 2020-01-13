34b809daa2229e7bac30cf44555f8941363c4d0c
Charles Oliveira <charlesww1@hotmail.com>
2020-01-13 20:12:30 -0300

    meee

--- funcaoEmQuaseTudo.js
+++
@@ -1,5 +0,0 @@
-console.log(typeof Object)
-
-class Produto{}
-    console.log(typeof Produto)
-    
--- usandoLet1.js
+++
@@ -1,7 +0,0 @@
-//let só funciona dentro de scopo
-var numero = 1
-{
-    let numero = 2
-    console.log('dentro =', numero)
-}
-    console.log('fora =', numero)
